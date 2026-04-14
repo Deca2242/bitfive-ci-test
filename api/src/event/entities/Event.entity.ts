c6aac3011218ae26f4ca660c8719f97a2c7a6d30
Cristian Peña <cristiandpm.2004@gmail.com>
2026-04-14 14:31:35 -0500

    feat (api):add Event entity with ORM decorators

---
+++ api/src/event/entities/Event.entity.ts
@@ -0,0 +1,51 @@
+import {Entity, Id, Column} from "../../core/orm/decorators/decorators.js"
+
+@Entity('Events')
+export class Event {
+
+    @Id()
+    id: string
+
+    @Column({ type: 'string', nullable: false })
+    name: string
+
+    @Column({ type: 'string' })
+    description: string
+
+    @Column({ type: 'date', nullable: false })
+    date: Date
+    
+    @Column({ type: 'string', nullable: false })
+    dateTime: Date
+
+    @Column({ type: 'boolean', default: false })
+    eventStatus: boolean
+
+    @Column({ type: 'number' })
+    maxCapacity: number
+
+    @Column({ type: 'boolean', default: false })
+    hasParking: boolean
+
+    @Column({ type: 'boolean', default: false })
+    parkingSlots: boolean
+
+
+    isActive: boolean
+
+    isDeleted: boolean
+
+    createdAt: Date
+
+    createdBy: string
+
+    updatedAt: Date
+
+    updatedBy: string
+
+    deletedAt: Date
+
+    deletedBy: string
+}
+
+export default Event
