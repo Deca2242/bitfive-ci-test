d49a6a18561d19192dfc320201fc349eb658f09b
Cristian <cmbarrera2242@gmail.com>
2026-04-14 20:13:30 -0500

    feat(event): add parkings route and enhance validation logic

--- api/src/event/controllers/Event.controller.ts
+++ api/src/event/controllers/Event.controller.ts
@@ -3,9 +3,11 @@ import { Controller } from "../../core/decorators/controller.decorator.js";
 import { Inject } from "../../core/decorators/inject.decorator.js";
 import { Delete, Get, Post, Put } from "../../core/decorators/route.decorator.js";
 import Validator from "../../core/utils/Validator.js";
+import { InvalidFormatError } from "../../core/errors/InvalidFormat.error.js";
 import { PERMISSIONS } from "../constants/authorities.js";
 import EventDTO from "../dtos/Event.dto.js";
 import EventService from "../services/Event.service.js";
+import ParkingDTO from "../../parking/dtos/Parking.dto.js";
 
 @Controller('/events')
 export class EventController {
@@ -14,6 +16,7 @@ export class EventController {
         @Inject(EventService)
         private eventService: EventService
     ) { }
+
     @Get('/')
     @Permissions([PERMISSIONS.EVENT.READ])
     async findAll(request, response) {
@@ -28,49 +31,86 @@ export class EventController {
         const { eventName } = request.params || {}
 
         Validator.required({ eventName })
-        
+
         const event = await this.eventService.findByName(eventName)
 
         return response.status(200).json(new EventDTO(event))
     }
 
+    @Get('/:eventId/parkings')
+    @Permissions([PERMISSIONS.EVENT.READ])
+    async findParkingsByEvent(request, response) {
+        const { eventId } = request.params || {}
+
+        Validator.required({ eventId })
+
+        const event = await this.eventService.findByIdWithParkings(eventId)
+
+        return response.status(200).json((event.parkings || []).map(p => new ParkingDTO(p)))
+    }
+
     @Post('/')
     @Permissions([PERMISSIONS.EVENT.CREATE])
     async create(request, response) {
-        const { name, description, date, dateTime, eventStatus, maxCapacity, hasParking, parkingSlots} = request.body
-
-        Validator.required({ name, description, date, dateTime, eventStatus, maxCapacity, hasParking, parkingSlots })
-
-        const event = await this.eventService.create({ name, description, date, dateTime, eventStatus, maxCapacity, hasParking, parkingSlots })
-
-        return response.status(201).json(new EventDTO(event))
-    }   
-
-    @Put('/:eventname')
-    @Permissions([PERMISSIONS.EVENT.UPDATE])
-    async update(request, response) {
-        const { eventname } = request.params
-        const { name, description, date, dateTime, eventStatus, maxCapacity, hasParking, parkingSlots } = request.body
+        const { name, description, location, date, dateTime, eventStatus, maxCapacity, hasParking } = request.body
 
         Validator
-        .required({ eventname })
-        .required({ name, description, date, dateTime, eventStatus, maxCapacity, hasParking, parkingSlots })
+            .required({ name, description, location, date, dateTime, maxCapacity })
+            .isDate({ date })
+            .isNumeric({ maxCapacity })
 
-        const event = await this.eventService.update(eventname, { name, description, date, dateTime, eventStatus, maxCapacity, hasParking, parkingSlots })
+        if (Number(maxCapacity) <= 0) {
+            throw new InvalidFormatError('maxCapacity debe ser mayor a 0')
+        }
+
+        const eventDate = new Date(date)
+        if (eventDate <= new Date()) {
+            throw new InvalidFormatError('La fecha del evento debe ser futura')
+        }
+
+        const event = await this.eventService.create({ name, description, location, date, dateTime, eventStatus, maxCapacity, hasParking })
+
+        return response.status(201).json(new EventDTO(event))
+    }
+
+    @Put('/:id')
+    @Permissions([PERMISSIONS.EVENT.UPDATE])
+    async update(request, response) {
+        const { id } = request.params
+        const { name, description, location, date, dateTime, eventStatus, maxCapacity, hasParking } = request.body
+
+        Validator.required({ id })
+
+        if (date) {
+            Validator.isDate({ date })
+            const eventDate = new Date(date)
+            if (eventDate <= new Date()) {
+                throw new InvalidFormatError('La fecha del evento debe ser futura')
+            }
+        }
+
+        if (maxCapacity !== undefined) {
+            Validator.isNumeric({ maxCapacity })
+            if (Number(maxCapacity) <= 0) {
+                throw new InvalidFormatError('maxCapacity debe ser mayor a 0')
+            }
+        }
+
+        const event = await this.eventService.update(id, { name, description, location, date, dateTime, eventStatus, maxCapacity, hasParking })
 
         return response.status(200).json(new EventDTO(event))
     }
 
-    @Delete('/:eventname')
+    @Delete('/:id')
     @Permissions([PERMISSIONS.EVENT.DELETE])
     async delete(request, response) {
-        const { eventname } = request.params
+        const { id } = request.params
 
-        Validator.required({ eventname })
+        Validator.required({ id })
 
-        await this.eventService.delete(eventname)
+        await this.eventService.delete(id)
 
-        return response.status(204).send()  
+        return response.status(204).send()
     }
 
 }
