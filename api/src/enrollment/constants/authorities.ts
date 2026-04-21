19c61905c69f8c00338e864cd81c5de24397d35d
Cristian Peña <cristiandpm.2004@gmail.com>
2026-04-21 14:24:15 -0500

    feat(enrollment): Crate permissions roles

---
+++ api/src/enrollment/constants/authorities.ts
@@ -0,0 +1,9 @@
+export const PERMISSIONS = {
+
+    EVENT: {
+        READ: 'ReadEnrollments',
+        CREATE: 'CreateEnrollments',
+        UPDATE: 'UpdateEnrollments',
+        DELETE: 'DeleteEnrollments',
+    }
+}
