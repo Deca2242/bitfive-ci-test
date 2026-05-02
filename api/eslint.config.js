ea70ab9f45a1b4569233b97073dc8b1745feb1a6
Cristian <cmbarrera2242@gmail.com>
2026-05-02 08:12:40 -0500

    Disable unsafe function type rule in API ESLint

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -18,7 +18,8 @@ export default tseslint.config(
     rules: {
       "no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      '@typescript-eslint/no-unsafe-function-type': 'off'
     },
   },
   
